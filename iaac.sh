e441c78cced56e513a7e7ab5f58c0e95d77daa80
rigoncalvesdias01 <71675970+rigoncalvesdias01@users.noreply.github.com>
2021-05-05 23:53:39 -0300

    Delete iaac.sh

--- iaac.sh
+++
@@ -1,5 +0,0 @@
-apt install apt-transport-https ca-certificates gnupg -y
-echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-apt update && sudo apt-get install google-cloud-sdk -y
-apt install google-cloud-sdk-app-engine-java -y
